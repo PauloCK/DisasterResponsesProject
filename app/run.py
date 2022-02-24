881f9d5aadf8cb8edac2afa381f8775a26775a28
PauloCK <paulockoff@gmail.com>
2022-02-24 00:44:01 +0000

    Changing paths to files in run.py

--- app/run.py
+++ app/run.py
@@ -26,11 +26,12 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/disaster_responses_db.db')
+# print(pd.read_sql("SELECT name FROM sqlite_master WHERE type='table';", engine))
+df = pd.read_sql_table('data/disaster_responses_tbl', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
